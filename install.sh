0b10718a06e7f983536baa7daeff2f6a02bc8c7b
Daniel Jost <info@daniel-jost.de>
2015-08-01 12:50:42 +0200

    new way to install docker

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 apt-get -qq -y update
-apt-get -qq -y dist-upgrade
-apt-get -qq -y install apt-transport-https
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-echo "deb https://get.docker.com/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
-apt-get -qq -y update
-apt-get -qq -y install lxc-docker
+apt-get -qq -y install linux-image-generic-lts-trusty
+wget -qO- https://get.docker.com/ | sh
