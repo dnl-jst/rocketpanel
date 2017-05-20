f92367dfb91d42fc879460a5a64a7818534da213
Daniel Jost <info@daniel-jost.de>
2017-05-20 23:13:55 +0200

    launch rocketpanel-control container

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ apt-get -qq -y install \
     linux-image-extra-virtual
 
 wget -qO- https://get.docker.com/ | sh
+
+docker pull dnljst/rocketpanel-control
+docker run -e "WEB_DOCUMENT_ROOT=/app/web" -v /opt/rocketpanel/:/rocketpanel -d -p 8443:443 --name rocketpanel-control dnljst/rocketpanel-control
